11f135155d5fb92bea27264146095f7f00389af6
ashu <ashupatil1357@gmail.com>
2024-04-30 15:37:41 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,15 @@
 import './App.css';
-import lake from "./image/lake.JPG";
 import GallaryFooter from './components/GallaryFooter';
-
-const imageData = [
-  {
-    id:1,
-    img:lake 
-  },
-  {
-    id:2,
-    img:lake
-  },
-  {
-    id:3,
-    img:lake
-  },
-  {
-    id:4,
-    img:lake
-  }
-]
+import GalleryHeader from './components/GalleryHeader';
+import GalleryBody from './components/GalleryBody';
 
 function App() {
   return (
     <div>
-      <header>
-        <img className="logo" src="https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/Kalvium-Logo.png" alt=""></img>
-        <nav>
-          <ul className='nav_links'>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>About</a></li>
-            <li><a href='#'>More</a></li>
-          </ul>
-
-        </nav>
-        <a className='cta' href='#'><button>Contact</button></a>
-      </header>
-
+      <GalleryHeader/>
       <div>
-        {
-          imageData.map(image=>{
-            return(
-              <div key={image.id} className="column">
-                <img src={image.img} alt=""></img>
-              </div>
-            )
-          })
-        }
+      <GalleryBody/>
       </div>
-      {/* adding footer component */}
       <GallaryFooter/> 
     </div>
   )
---
+++ src/components/GalleryBody.jsx
@@ -0,0 +1,36 @@
+import lake from "../image/lake.JPG";
+
+
+const imageData = [
+    {
+      id:1,
+      img:lake 
+    },
+    {
+      id:2,
+      img:lake
+    },
+    {
+      id:3,
+      img:lake
+    },
+    {
+      id:4,
+      img:lake
+    }
+  ]
+  
+export default function(){
+    return(
+          imageData.map(image=>{
+            return(
+              <div key={image.id} className="column">
+                <img src={image.img} alt=""></img>
+              </div>
+            )
+          })
+    )
+  }
+
+  import React from 'react'
+  
